f1ffc512b50baaf9daf77bcced2472c82349e2f6
T-LEPARE <tristan.lepare@gmail.com>
2024-03-05 20:00:11 +0100

    fix Position

--- PROJET/src/Position.cpp
+++ PROJET/src/Position.cpp
@@ -1,13 +1,21 @@
 #include "Position.h"
 
-
-
 Position::Position(){
-    x=0;
-    y=0;
 }
 
 Position::Position (float x, float y) {
-    this->x = x;
-    this->y = y;
+  this->xy.x = x;
+  this->xy.y = y;
+}
+
+float Position::getX() {
+  return xy.x;
+}
+
+float Position::getY() {
+  return xy.y;
+}
+
+positions Position::getPos(){
+  return xy;
 }
--- PROJET/src/Position.h
+++ PROJET/src/Position.h
@@ -1,22 +1,40 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-struct Position{
-    float x;
-    float y;
+struct positions
+{
+  float x;
+  float y;
+};
 
-    /**
-     @brief constructeur de position
-     @param x position x
-     @param x position y
-    */
-   Position();
-   Position (float x, float y);
-    
-    /**
-     * @return renvoie une position 
-    */
-   Position getpos();
+
+class Position{
+  private:
+  positions xy;
+
+  /**
+   * @brief constructeur de position
+   * @param x position x
+   * @param x position y
+   */
+  Position();
+  Position (float x, float y);
+
+  public:
+  /**
+   * @return renvoie la position X
+   */
+  float getX();
+
+  /**
+   * @return renvoie la position Y
+   */
+  float getY();
+
+  /**
+   * @return renvoie une position 
+   */
+  positions getPos();
 };
 
 #endif
