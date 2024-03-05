91ea0e3d37d58d7ea68142b07b0932c5f346d081
louis444 <louis.duflo@gmail.com>
2024-03-05 11:22:57 +0100

    Player/position.h

---
+++ PROJET/src/Playership.h
@@ -0,0 +1,44 @@
+#ifndef PLAYERSHIP_H
+#define PLAYERSHIP_H
+
+#include <Position.h>
+
+class Player {
+private:
+    Position pos;
+    float speed;
+
+public:
+    /**
+     @brief constructeur du joueur (vaisseau)
+     @param x  position x 
+     @param y  position y
+     @param HP vie du joueur 
+    */
+    Player InitPlayer(float x, float y, int HP);
+
+    /**
+     @return retourne la position du joueur
+    */
+    Position getPos();
+
+    /**
+     @return retourne le nombre de point de vie du joueur
+    */
+    int getHP();
+
+    /**
+     @brief les déplacements du joueur 
+    */
+    void movement();
+
+    /**
+     @brief Vérifie si le joueur a été touché ou non
+     @param  P position d'un invader ou d'un projectile
+    */
+    bool CheckCollision(Position P);
+};
+
+
+
+#endif
---
+++ PROJET/src/Position.h
@@ -0,0 +1,17 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+struct Position{
+    float x;
+    float y;
+
+    /**
+     @brief constructeur de position
+     @param x position x
+     @param x position y
+     @return retourne la position (x,y)
+    */
+    Position (float x, float y);
+};
+
+#endif
