fa5afd5395e1cd286d8ebfef0c6a1627ca32fe4b
T-LEPARE <tristan.le-pare@etu.univ-lyon1.fr>
2024-03-12 12:03:32 +0100

    weapons fix

--- PROJET/src/Projectile.h
+++ PROJET/src/Projectile.h
@@ -13,11 +13,7 @@ struct weapon {
 //Le delta temps pour modifier la vitesse des projectiles
 const float dt = 0.1;
 
-std::map<std::string, weapon> weapons {
-    // {nom,{vitesse,damage}}
-    {"mitraillette",{100, 1}},
-    {"railgun", {1,100}},
-};
+const std::map<std::string, weapon> weapons;
 
 class Projectile {
 
