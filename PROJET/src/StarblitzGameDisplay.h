3d68e5f79386df0b40a7d551208d78c191274e27
T-LEPARE <tristan.lepare@gmail.com>
2024-03-13 13:30:57 +0100

    séparation GameLogic/GameDisplay

--- PROJET/src/StarblitzGameDisplay.h
+++ PROJET/src/StarblitzGameDisplay.h
@@ -2,10 +2,6 @@
 #define STARBLITZGAMEDISPLAY_H
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include "Invaders.h"
-#include "Playership.h"
-#include "Position.h"
-#include "ScoreSystem.h"
 
 const int DIMH = 540;
 const int DIMW = 960;
--- PROJET/src/StarblitzGameLogic.h
+++ PROJET/src/StarblitzGameLogic.h
@@ -1,6 +1,9 @@
 #ifndef STARBLITZGAMELOGIC_H
 #define STARBLITZGAMELOGIC_H
-
+#include "Invaders.h"
+#include "Playership.h"
+#include "Position.h"
+#include "ScoreSystem.h"
 
 
 #endif
