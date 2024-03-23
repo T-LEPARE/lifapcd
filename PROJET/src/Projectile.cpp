1c4c0f3b195b6e910eff77dab6bfbf78d595ba53
T-LEPARE <tristan.lepare@gmail.com>
2024-03-23 16:24:43 +0100

    Projectiles error fix

--- PROJET/src/Projectile.cpp
+++ PROJET/src/Projectile.cpp
@@ -12,7 +12,6 @@ Projectile::Projectile(Position pos,std::map<std::string, weapon> weapons,std::s
     this->pos = pos; // Ici il faudrait faire un getPos() soit pour l'invader soit pour le Playership, mais comme la position sera l'angle supérieur gauche, il faudra modifier pour que ça parte de la moitié du mec...
     this->damage = weapons[weaponName].damage;
     this->projectileSpeed = weapons[weaponName].projectileSpeed;
-
     this->direction = Position(0,-1); // la direction initiale du tir, vers le bas
 }
 
